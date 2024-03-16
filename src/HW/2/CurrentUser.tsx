86f3cd69711f2b39e0af1541b95c11df85c46af6
kurt2019 <artemkurt@ukr.net>
2024-03-16 18:25:50 +0200

    GitHub Pages

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -10,5 +10,6 @@ export const CurrentUser = (props: CurrentUserPropsType) => {
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
       {props.user.address.street}, {props.user.address.city}
     </li>
+
   );
 };
